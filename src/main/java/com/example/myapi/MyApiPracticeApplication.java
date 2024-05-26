74b8a2bebb97375934d04e87732edc48c7060abe
Fukuda57 <joichiro.57.f@icloud.com>
2024-05-26 11:08:52 +0900

    Initial Commit

---
+++ src/main/java/com/example/myapi/MyApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiPracticeApplication.class, args);
+	}
+
+}
